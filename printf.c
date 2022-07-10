0bd5e20ce6b7b1c7be4cfe7dad4ccf7a8148a73a
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-07-10 18:59:44 +0200

    Create printf.c

---
+++ printf.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _printf - Function that sends formatted output to stdout.
+ * @format: Character string. Is composed of zero or more directives.
+ * Return: Print a new line at the end of your function.
+ */
+int _printf(const char *format, ...)
+{
+	va_list arg;
+	int counter = 0;
+
+	forma_t p[] = {
+		{"c", op_c},
+		{"s", op_s},
+		{"d", op_i},
+		{"i", op_i},
+		{NULL, NULL}
+		};
+
+	va_start(arg, format);
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+	counter = format_h(format, p, arg);
+
+	va_end(arg);
+	return (counter);
+}
